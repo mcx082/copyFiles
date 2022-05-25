2eeb9a0ce51f2e2db534b40650730ac4821f5f6d
mcx082 <mcx082@wp.pl>
2022-05-25 14:30:06 +0200

    oki

--- BlobTriggerFunction1/index.js
+++ BlobTriggerFunction1/index.js
@@ -1,3 +1,11 @@
 module.exports = async function (context, myBlobXML) {
     context.log("JavaScript blob XML trigger function processed blob \n Blob:", context.bindingData.blobTrigger, "\n Blob Size:", myBlobXML.length, "Bytes");
+
+    if (myBlobXML) {
+        context.bindings.outputDocument = JSON.stringify({
+            // create a random ID
+            id: new Date().toISOString() + Math.random().toString().substr(2,8),
+            name: myBlobXML
+        });
+    }
 };
